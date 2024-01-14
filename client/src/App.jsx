980053517cccd57519ee916718f1d984308cad7f
akhribabderahmane <la_akhrib@esi.dz>
2024-01-14 01:43:10 +0100

    aadded:verification code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import ForgotPassword from "./pages/forgetPassword";
+import Verification from "./pages/verification"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forget-password" element={<ForgotPassword />} />
+          <Route path="/verification-code" element={<Verification />} />
         </Routes>
       </main>
     </Router>
