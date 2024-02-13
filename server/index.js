abe8c438bac64d28cafaac5dc1e5f7f9cfa3226d
akhribabderahmane <la_akhrib@esi.dz>
2024-02-14 00:48:05 +0100

    added socket

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import dotenv from "dotenv"
+import { createServer} from 'http'
+import {Server} from 'socket.io'
 import { connectDatabase } from './config/db.config.js';
 import morgan from 'morgan'
 import userRouter from './routers/user.Router.js';
 
-
 const app = express()
 const port = 3000
+const server=createServer(app);
+const io=new Server(server);
 dotenv.config();
 connectDatabase();
 
@@ -14,6 +17,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/user",userRouter)
+
+io.on('connection',(socket)=>{
+   console.log('a user connected');
+   socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+})
 
  
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
